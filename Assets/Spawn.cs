968af8d94b2684cafcb42c555320a5ab3dee85d8
Boogaga <napolinkinpark3r@gmail.com>
2012-07-04 23:27:26 +0400

    some changes in Spawn script

--- Assets/Spawn.cs
+++ Assets/Spawn.cs
@@ -4,21 +4,24 @@ using System.Collections;
 public class Spawn : MonoBehaviour {
 
 public GameObject prefab;
-
-public void Update(){
+	
+	void spawn(){
 		float prefabX = prefab.transform.position.x;
 		float prefabZ = prefab.transform.position.z;
 
-		for(int i=0; i<20;){
+		for(int i=0; i<10;){
 			float cloneX = Random.Range(prefabX-100.0f,prefabX+100.0f);
 			float cloneZ = Random.Range(prefabZ-100.0f,prefabZ+100.0f);
 
-			float distance = Mathf.Sqrt(Mathf.Pow(cloneX-prefabX,2)+Mathf.Pow(cloneZ-prefabZ,2));
+			Vector3 distance = new Vector3(cloneX-prefabX,0,cloneZ-prefabZ);
 
-			if(distance>=50.0){
-			Instantiate(prefab, new Vector3(cloneX, 0, cloneZ), Quaternion.identity);
+			if(distance.magnitude>=20.0){
+				Instantiate(prefab, new Vector3(cloneX, 0, cloneZ), Quaternion.identity);
 				i++;
 				}
 			}
+		}
+	public void Start(){
+		spawn();
 		}
 	}
